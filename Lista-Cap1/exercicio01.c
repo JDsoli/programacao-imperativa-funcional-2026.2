3d67dbf95a67ef8e6b21d1f51f54e1be5b1c75cf
Jardel <jardelsoliveirajr@gmail.com>
2026-09-24 12:37:22 -0300

    exer_esqueleto

---
+++ Lista-Cap1/exercicio01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int dataNasci = 1996;
+
+    printf("Ano do meu nascimento %d \n",dataNasci);
+
+    system("PAUSE");
+    return 0;
+}
---
+++ Lista-Cap1/exercicio02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float euler = 2.71828f;
+
+    printf("o valor em tres casas decimais e %.3f\n", euler);
+
+    system("PAUSE");
+    return 0;
+}
